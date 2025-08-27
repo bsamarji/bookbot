6162be52b586cf93d3963d1abf5fdfe817005d43
bsamarji <bensamarji5637@gmail.com>
2025-08-28 00:30:46 +0100

    updated main.py with book text function

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+def get_book_text(file):
+    with open(file) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    file = "books/frankenstein.txt"
+
+    book = get_book_text(file)
+    print(book)
+
+main()
