23dea146d706e9bc8ff78172b1b414376ad22cc2
bsamarji <bensamarji5637@gmail.com>
2025-08-29 19:27:48 +0200

    added bookbot report.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_count, char_count
+from stats import word_count, char_count, sort_chars
 
 def get_book_text(file):
     with open(file) as f:
@@ -12,9 +12,18 @@ def main():
     file = "books/frankenstein.txt"
     book = get_book_text(file)
     num_words = word_count(book)
-    print(f"{num_words} words found in the document")
     char_set = get_unique_chars(book)
     char_dict = char_count(char_set, book)
-    print(char_dict)
+    sorted_chars = sort_chars(char_dict)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for char in sorted_chars:
+        if char["char"].isalpha():
+            print(f"{char['char']}: {char['num']}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -8,3 +8,19 @@ def char_count(char_list, book_text):
     for l in char_list:
         char_info[l] = book_text.count(l)
     return char_info
+
+def sort_on(items):
+    return items["num"]
+
+def sort_chars(char_dict):
+    char_list = []
+
+    for key in char_dict:
+        chars = {
+            "char": key,
+            "num": char_dict[key]
+        }
+        char_list.append(chars)
+
+    char_list.sort(reverse=True, key=sort_on)
+    return char_list
